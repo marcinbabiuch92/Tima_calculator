21cfe731ca33abb61f7bb1b2964e6537ea243ee3
Marcin Babiuch <marcinbabiuch92@gmail.com>
2021-10-17 21:34:19 +0200

    after cleaning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
  # This entrypoint file to be used in development. Start by reading README.md
 from time_calculator import add_time
-from unittest import main
 
 
 print(add_time("11:43 PM", "24:20", "tueSday"))
--- time_calculator.py
+++ time_calculator.py
@@ -14,11 +14,7 @@ def add_time(start, duration, weekday=''):
         hour = '12'
     minutes = str((int(time_b[1]) + int(time_m[1])) % 60)
     cycle = (int(time_m[0]) - (int(time_m[0]) % 12)) / 12
-
-    # counts how many days are in duration
     day_counter = 0
-    # day_counter = (int(time_m[0]) - (int(time_m[0]) % 24)) / 24
-    # print("day counter1: " + str(int(day_counter)))
 
     rest = int(time_m[0]) % 12
 
@@ -29,43 +25,36 @@ def add_time(start, duration, weekday=''):
             if day_part == 'AM':
                 day_part = 'PM'
             else:
+                day_counter += 1
                 day_part = 'AM'
                 day = " (next day)"
-                day_counter = day_counter + 1
-                # print("day counter2: " + str(int(day_counter)))
 
     # determining what part of day it is
     if (int(time_b[0]) + rest) >= 12:
         if day_part == 'AM':
             day_part = 'PM'
         else:
+            day_counter += 1
             day_part = 'AM'
-            day_counter = day_counter + 1
             day = " (next day)"
-            # print("day counter3: " + str(int(day_counter)))
 
     for time in range(0, int(cycle)):
         if day_part == 'AM':
             day_part = 'PM'
         else:
-            day_part = 'AM'
             day_counter += 1
+            day_part = 'AM'
             day = ' (next day)'
 
     if base[1] == 'PM' and day_counter > 1:
         day = ' (' + str(int(day_counter)) + ' days later)'
-        # print("day counter4: " + str(int(day_counter)))
     elif base[1] == 'AM' and day_counter > 1:
         day = ' (' + str(int(day_counter)) + ' days later)'
-        # print("day counter5: " + str(int(day_counter)))
-
-    # print("Days: " + str(int(day_counter)))
 
     # this loop move through days thanks to day_counter
     index = day_counter % 7
     for dayname in week:
         if weekday == '':
-            weekday = ''
             break
         if weekday.lower() == dayname.lower():
             weekday = ', ' + str(week[(week.index(dayname) + int(index)) % 7])
